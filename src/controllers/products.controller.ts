4c3444e8acbe1c8b37b3be8cde3e86b63cb31134
Vitor Madeira <vitorsoutom@hotmail.com>
2026-09-05 11:19:09 -0300

    feat: add validation for product filters in listProducts controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,9 +1,13 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { ProductFilters } from "../types";
 import { getProducts } from "../services/products.service";
+import { productFiltersSchema } from "../utils/validators";
 
-export const listProducts = async (request: FastifyRequest<{ Querystring: ProductFilters }>, reply: FastifyReply) => {
-
-  const result = await getProducts(request.query);
+export const listProducts = async (
+  request: FastifyRequest<{ Querystring: ProductFilters }>,
+  reply: FastifyReply,
+) => {
+  const filters = productFiltersSchema.parse(request.query);
+  const result = await getProducts(filters as ProductFilters);
   reply.send(result);
-}
+};
