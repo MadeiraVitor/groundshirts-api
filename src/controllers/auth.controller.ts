6390df9b3c04f367994386f1de6f4bc6ed0f0fa8
Vitor Madeira <vitorsoutom@hotmail.com>
2026-09-05 11:19:01 -0300

    feat: add validation for user registration and login requests

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,15 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import type { AuthRequest, RegisterRequest } from "../types";
 import { loginUser, registerUser } from "../services/auth.service";
+import { loginSchema, registerSchema } from "../utils/validators";
 
 export const register = async (
   request: FastifyRequest,
   reply: FastifyReply,
 ) => {
-  const user = await registerUser(request.body as RegisterRequest, reply);
+  const validation = registerSchema.parse(request.body as RegisterRequest);
+
+  const user = await registerUser(validation, reply);
 
   const token = request.server.jwt.sign({ userId: user?.id });
 
@@ -20,9 +23,11 @@ export const login = async (
   request: FastifyRequest<{ Body: AuthRequest }>,
   reply: FastifyReply,
 ) => {
-  const user = await loginUser(request.body);
+  const validation = loginSchema.parse(request.body as AuthRequest);
+
+  const user = await loginUser(validation);
 
   const token = request.server.jwt.sign({ userId: user.id });
 
-  reply.send({ user, token });
+  reply.status(200).send({ user, token });
 };
