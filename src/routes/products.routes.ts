c710e2cfae24415a7b49441effdbdb6a90ecc25d
Vitor Madeira <vitorsoutom@hotmail.com>
2026-09-07 09:53:24 -0300

    feat: add getProduct endpoint with schema validation for retrieving a product by ID

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { listProducts } from "../controllers/products.controller";
+import { getProduct, listProducts } from "../controllers/products.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 
 export default async function productRoutes(fastify: FastifyInstance) {
@@ -91,5 +91,79 @@ export default async function productRoutes(fastify: FastifyInstance) {
       },
     },
     listProducts,
+  );
+
+  fastify.get(
+    "/:id",
+    {
+      schema: {
+        tags: ["Products"],
+        description: "Obtém um produto pelo ID",
+        params: {
+          type: "object",
+          properties: {
+            id: { type: "number", description: "ID do produto" },
+          },
+          required: ["id"],
+        },
+        response: {
+          200: {
+            description: "Produto encontrado",
+            type: "object",
+            properties: {
+              id: { type: "number" },
+              name: { type: "string" },
+              price: { type: "number" },
+              createdAt: { type: "string" },
+              description: { type: "string" },
+              stock: { type: "number" },
+              sizes: {
+                type: "array",
+                items: { type: "string" },
+              },
+              images: {
+                type: "array",
+                items: { type: "string", format: "uri" },
+              },
+              colors: {
+                type: "array",
+                items: { type: "string" },
+              },
+              slug: { type: "string" },
+              active: { type: "boolean" },
+              updatedAt: { type: "string" },
+              categoryId: { type: "number" },
+              category: {
+                type: "object",
+                properties: {
+                  id: { type: "number" },
+                  name: { type: "string" },
+                  slug: { type: "string" },
+                  description: { type: "string" },
+                  active: { type: "boolean" },
+                  createdAt: { type: "string", format: "date-time" },
+                  updatedAt: { type: "string", format: "date-time" },
+                },
+              },
+            },
+          },
+          400: {
+            description: "Requisição inválida",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+          401: {
+            description: "Não autorizado",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    getProduct,
   );
 }
