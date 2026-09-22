00d849e8705f778e9eb40ac7eb2c7f8cee675242
Vitor Madeira <vitorsoutom@hotmail.com>
2026-09-22 11:10:55 -0300

    feat: implement createStripeCheckoutService for handling Stripe checkout sessions

---
+++ src/services/stripe.service.ts
@@ -0,0 +1,46 @@
+import "dotenv/config";
+import Stripe from "stripe";
+
+interface OrderItems {
+  id: number;
+  name: string;
+  unitPrice: number;
+  quantity: number;
+}
+
+interface CreateStripeCheckoutServiceRequest {
+  products: OrderItems[];
+}
+
+export const createStripeCheckoutService = async ({
+  products,
+}: CreateStripeCheckoutServiceRequest) => {
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error(
+      "STRIPE_SECRET_KEY is not defined in the environment variables.",
+    );
+  }
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2025-02-24.acacia",
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    line_items: products.map((product) => ({
+      price_data: {
+        currency: "brl",
+        unit_amount: Math.round(product.unitPrice * 100),
+        product_data: {
+          name: product.name,
+        },
+      },
+      quantity: product.quantity,
+    })),
+  });
+
+  return {
+    sessionId: session.id,
+  };
+};
